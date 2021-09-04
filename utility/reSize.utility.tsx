18f8dc1209171a47eccc59740d7feb1be6379063
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2021-09-04 10:45:01 +0530

    -layout

--- utility/reSize.utility.tsx
+++ utility/reSize.utility.tsx
@@ -1,8 +1,8 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function useWindowSize() {
     const [size, setSize] = useState([0, 0]);
-    useLayoutEffect(() => {
+    useEffect(() => {
         function updateSize() {
             setSize([window.innerWidth, window.innerHeight]);
         }
